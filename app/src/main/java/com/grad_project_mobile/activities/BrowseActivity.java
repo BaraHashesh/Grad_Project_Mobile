8d8a56f62029c8524b7165e228597ccb5f12f953
BaraHashesh <1140236@student.birzeit.edu>
2018-09-09 14:31:09 +0300

    finishing the discovery procedure, and link the main activity and the server select activity together

---
+++ app/src/main/java/com/grad_project_mobile/activities/BrowseActivity.java
@@ -0,0 +1,15 @@
+package com.grad_project_mobile.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.grad_project_mobile.R;
+
+public class BrowseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse);
+    }
+}
